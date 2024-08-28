bc2c92e33d2ed81fb91a90e9284e60ceeb64d69e
Sarakh9an <141704230+Sarakh9an@users.noreply.github.com>
2024-08-28 10:06:25 +0530

    Update pggg.py

--- pggg.py
+++ pggg.py
@@ -9,7 +9,7 @@ from docx.enum.text import WD_ALIGN_PARAGRAPH
 
 # Initialization block
 keywords_list = ["Human Rights", "Illegal occupied", "Anti-India", "Hindu", "Supremacy", "Nationalist", "India", "USA", "China", "Bangladesh", "UK", "Lanka", "Afghanistan", "Militant", "Khalistan", "terror"]
-journal_list = ["washingtonpost.com", "nytimes.com", "thetimes.co.uk", "ft.com"]
+journal_list = ["ft.com"]
 language_list = ["eng", "hin"]
 topics = ["Geo Politics", "Military", "Development", "Health", "Business"]
 ignore_topic_list = ['hockey', 'cricket', "Bollywood", "Hollywood", "Box office", "Asia Cup", "Games", "sport", "Fashion"]
